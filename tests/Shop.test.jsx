41ff1fa2b4dd14f36fb4c1b04e8a08364032ee3c
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2026-03-26 17:52:35 +0000

    change input field to div in ShopItem and add tests to shop for changing quantities

--- tests/Shop.test.jsx
+++ tests/Shop.test.jsx
@@ -5,12 +5,13 @@ import userEvent from "@testing-library/user-event";
 import routes from "../src/routes";
 import App from "../src/App";
 
-describe("ShopItem", () => {
+// maybe try using a set up function
+
+describe("Shop", () => {
   it("loads items on page load", async () => {
     const router = createMemoryRouter(routes, {
       initialEntries: ["/shop"],
     });
-
     render(<RouterProvider router={router} />);
 
     expect(screen.getByText("Loading...")).toBeInTheDocument();
@@ -18,20 +19,60 @@ describe("ShopItem", () => {
       expect(screen.getByText("Mens Cotton Jacket")).toBeInTheDocument();
     });
   });
-  it("changes basket quantity on button click", async () => {
+  it("changes basket quantity when adding item to basket", async () => {
     const user = userEvent.setup();
 
     const router = createMemoryRouter(routes, {
       initialEntries: ["/shop"],
     });
-
     render(<RouterProvider router={router} />);
 
-    const button = screen.getAllByRole("button", { name: "Add to Basket" })[0];
-    const basket = screen.getAllByRole("link")[2];
-    await user.click(button);
+    const addToBasket = screen.getAllByRole("button", {
+      name: "Add to Basket",
+    })[0];
+    expect(addToBasket).toBeInTheDocument();
 
+    const basket = screen.getAllByRole("link")[2];
+    await user.click(addToBasket);
     expect(basket.textContent).toMatch("Basket 1");
-    expect(button).toBeInTheDocument();
+  });
+  it("changes item quantity when using plus and minus buttons", async () => {
+    const user = userEvent.setup();
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/shop"],
+    });
+    render(<RouterProvider router={router} />);
+
+    const addButton = screen.getAllByRole("button", { name: "+" })[0];
+    const minusButton = screen.getAllByRole("button", { name: "-" })[0];
+    const quantity = screen.getAllByText("1")[0];
+
+    expect(quantity).toBeInTheDocument();
+
+    await user.click(addButton);
+    expect(quantity.textContent).toMatch("2");
+    await user.click(minusButton);
+    expect(quantity.textContent).toMatch("1");
+
+  });
+  it("changes basket quantity when adding multiple items to basket", async () => {
+    const user = userEvent.setup();
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/shop"],
+    });
+    render(<RouterProvider router={router} />);
+
+    const addToBasket = screen.getAllByRole("button", {
+      name: "Add to Basket",
+    })[0];
+    const addButton = screen.getAllByRole("button", { name: "+" })[0];
+    expect(addButton).toBeInTheDocument();
+
+    const basket = screen.getAllByRole("link")[2];
+    await user.click(addButton);
+    await user.click(addToBasket);
+    expect(basket.textContent).toMatch("Basket 2");
   });
 });
