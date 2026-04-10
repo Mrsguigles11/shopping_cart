04794008ae8b0193884eea91225a35f9e4f33e79
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2026-04-10 20:34:00 +0100

    add css for different screen formats

--- src/components/basket/basketItem/BasketItem.jsx
+++ src/components/basket/basketItem/BasketItem.jsx
@@ -3,23 +3,20 @@ import styles from "./BasketItem.module.css";
 export function BasketItem({item, removeItem, editQuantity}) {
   return (
     <div className={styles.container}>
-      <div>
-        <div className={styles.content_container}>
-          <img
-            src={item.image}
-            alt={item.title + " image"}
-            className={styles.img}
-          />
-          <div className={styles.details}>
-            <h2>{item.title}</h2>
-            <div>
-              <div className={styles.quantity}>
-                <button onClick={() => editQuantity("decrease", item.id)} className={styles.quantity_button}>-</button>
-                <div>{item.quantity}</div>
-                <button onClick={() => editQuantity("increase", item.id)} className={styles.quantity_button}>+</button>
-              </div>
-              <button className={styles.remove_button} onClick={() => removeItem(item.id, item.quantity)}>Remove</button>
+      <div className={styles.content_container}>
+        <img
+          src={item.image}
+          alt={item.title + " image"}
+        />
+        <div className={styles.details}>
+          <h2>{item.title}</h2>
+          <div>
+            <div className={styles.quantity}>
+              <button onClick={() => editQuantity("decrease", item.id)} className={styles.quantity_button}>-</button>
+              <div>{item.quantity}</div>
+              <button onClick={() => editQuantity("increase", item.id)} className={styles.quantity_button}>+</button>
             </div>
+            <button className={styles.remove_button} onClick={() => removeItem(item.id, item.quantity)}>Remove</button>
           </div>
         </div>
       </div>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,7 +7,7 @@ export function Home() {
     <div className={`${styles.home} content`}>
       <div className={styles.home_content}>
         <div className={styles.home_content_txt}>
-          <h2 className={styles.header}>Welcome to the Odin Shop</h2>
+          <h2 className={styles.title}>Welcome to the Odin Shop</h2>
           <i>
             Come check out our totally real items in this totally real shop
           </i>
